5fc8491b24e12291451eb63ce702955a9af35e43
Masa <59505095+Masa005@users.noreply.github.com>
2020-02-25 23:37:20 +0900

    デプロイ関連

--- sample_app/settings/base.py
+++ sample_app/settings/base.py
@@ -11,25 +11,21 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from .local_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
-
-ADMINS
-SERVER_EMAIL
 
 # Application definition
 INSTALLED_APPS = [
@@ -82,11 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sample_app.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
