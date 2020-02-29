5ad57c21c3922285f1815bc2b528313a904db49c
Masa <59505095+Masa005@users.noreply.github.com>
2020-02-29 16:18:16 +0900

    MIDDLEWARE WhiteNoise記載位置変更

--- sample_app/settings/base.py
+++ sample_app/settings/base.py
@@ -45,13 +45,13 @@ AUTH_USER_MODEL = 'account.UserModel'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'urls'
